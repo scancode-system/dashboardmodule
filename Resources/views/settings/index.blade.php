61bcff469fd4f17e7e0c8ad7e0d366cc6daab736
Your Name <you@example.com>
2019-11-18 01:37:51 -0300

    .

--- Resources/views/settings/index.blade.php
+++ Resources/views/settings/index.blade.php
@@ -7,30 +7,13 @@
 			<div class="card-header">
 				<nav>
 					<div class="nav nav-tabs" role="tablist">
-						<a class="nav-item nav-link {{ ($tab=='events')?'active':'' }}" href="{{ route('settings.index', 'events') }}" >Eventos</a>
-						<a class="nav-item nav-link {{ ($tab=='requests')?'active':'' }}" href="{{ route('settings.index', 'requests') }}" >Pedidos</a>
-						<a class="nav-item nav-link {{ ($tab=='clients')?'active':'' }}" href="{{ route('settings.index', 'clients') }}">Clientes</a>
-						<a class="nav-item nav-link {{ ($tab=='prints')?'active':'' }}" href="{{ route('settings.index', 'prints') }}">Impressão</a>
-						@loader(['loader_path' => 'settings.nav_tab'])
-
+						@loader(['loader_path' => 'settings.nav'])
 					</div>
 				</nav>
 			</div>
 			<div class="card-body">
 				<div class="tab-content">
-					@loader(['loader_path' => 'settings.tab_content'])
-					<div class="tab-pane {{ ($tab=='events')?'show active':'' }}" >
-						@include('dashboard::settings.tab.events.events')
-					</div>
-					<div class="tab-pane {{ ($tab=='requests')?'show active':'' }}" >
-						@include('dashboard::settings.tab.requests.requests')
-					</div>
-					<div class="tab-pane {{ ($tab=='clients')?'show active':'' }}" >
-						@include('dashboard::settings.tab.clients.clients')
-					</div>
-					<div class="tab-pane {{ ($tab=='prints')?'show active':'' }}" >
-						@include('dashboard::settings.tab.prints.prints')
-					</div>
+					@loader(['loader_path' => 'settings.body'])
 				</div>
 			</div>
 		</div>
