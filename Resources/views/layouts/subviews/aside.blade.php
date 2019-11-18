11d2b484186397bdbfa21756d1e4371054b9f97c
Leonardo <leonardoborgesavasconcelos@gmail.com>
2019-11-18 16:25:39 -0300

    .

--- Resources/views/layouts/subviews/aside.blade.php
+++ Resources/views/layouts/subviews/aside.blade.php
@@ -35,34 +35,7 @@
             </div>
         </div>
         <div class="tab-pane" id="messages" role="tabpanel">
-            <div class="list-group list-group-accent">
-                <div class="list-group-item list-group-item-accent-secondary bg-light text-center font-weight-bold text-muted text-uppercase small">Pedidos</div>
-
-                <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-accent-dark list-group-item-divider list-group-item-action">Simples</a>
-                <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-accent-dark list-group-item-divider list-group-item-action">Detalhado</a>
-                
-            </div>
-            <div class="list-group list-group-accent">
-                <div class="list-group-item list-group-item-accent-secondary bg-light text-center font-weight-bold text-muted text-uppercase small">Items</div>
-
-                <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-accent-dark list-group-item-divider list-group-item-action">Simples</a>
-                <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-accent-dark list-group-item-divider list-group-item-action">Detalhado</a>
-                
-            </div>
-            <div class="list-group list-group-accent">
-                <div class="list-group-item list-group-item-accent-secondary bg-light text-center font-weight-bold text-muted text-uppercase small">Produtos</div>
-
-                <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-accent-dark list-group-item-divider list-group-item-action">Simples</a>
-                <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-accent-dark list-group-item-divider list-group-item-action">Detalhado</a>
-                
-            </div>
-            <div class="list-group list-group-accent">
-                <div class="list-group-item list-group-item-accent-secondary bg-light text-center font-weight-bold text-muted text-uppercase small">Representantes</div>
-
-                <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-accent-dark list-group-item-divider list-group-item-action">Simples</a>
-                <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-accent-dark list-group-item-divider list-group-item-action">Detalhado</a>
-                
-            </div>
+            @loader(['loader_path' => 'aside.report'])
         </div>
         <div class="tab-pane p-3" id="settings" role="tabpanel">
             <h6>Backup</h6>
