b501218df408a51c5bada0f8a8addcaba114343c
Your Name <you@example.com>
2019-11-24 00:10:37 -0300

    .

--- Resources/views/loader/settings/tables/tr.blade.php
+++ Resources/views/loader/settings/tables/tr.blade.php
@@ -1,14 +1,14 @@
 <tr>
-	<td>
+	<td class="align-middle">
 		{{ $widget->name }}
 	</td>
-	<td class="text-center">
+	<td class="text-center align-middle">
 		<span class="badge badge-{{ ($widget->visible)?'success':'danger' }} badge-pill py-1 px-4">{{ ($widget->visible)?'Sim':'Não' }}</span>
 	</td>
-	<td class="text-center">
+	<td class="text-center align-middle">
 		{{ $widget->columns }} Colunas
 	</td>
-	<td class="text-right">
+	<td class="text-right align-middle">
 			<a href="#" class="btn btn-secondary" data-toggle="modal" data-target="#widgets_edit_{{ $widget->id }}"><i class="fa fa-edit"></i></a>
 	</td>
 	@include('dashboard::loader.settings.modals.widgets_edit', ['modal_id' => 'widgets_edit_'.$widget->id])
